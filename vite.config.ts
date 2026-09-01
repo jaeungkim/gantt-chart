87070567f7859ebe5bb7d8d4d082cc05d54534da
jaeungkim <jaewoongkim95@gmail.com>
2026-09-01 15:50:20 +0900

    chore: drop unused vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
     react(),
-    svgr(),
     tsconfigPaths(),
     dts({
       include: ['src'],
